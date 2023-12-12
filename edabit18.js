586a34b9c19fe91c34a42695e1ddc7f0e2caa034
Ashique Ali K <ashiquealikrz9@gmail.com>
2023-12-12 17:34:19 +0530

    Reverse the Odd Length Words

---
+++ edabit18.js
@@ -0,0 +1,14 @@
+function reverseOdd(str) {
+  const words = str.split(" ");
+  const reversedWords = words.map((word) => {
+    if (word.length % 2 === 1) {
+      return word.split("").reverse().join("");
+    } else {
+      return word;
+    }
+  });
+
+  return reversedWords.join(" ");
+}
+
+console.log(reverseOdd("Bananas"));
