d5de5feea378609864872801134a70489141b852
Ashique Ali K <ashiquealikrz9@gmail.com>
2023-12-28 21:30:22 +0530

    Boomerang

---
+++ edabit26.js
@@ -0,0 +1,14 @@
+function countBoomerangs(arr) {
+    let count = 0;
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i] === arr[i + 2] && arr[i] !== arr[i + 1]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const array1 = [3, 7, 3];
+console.log(countBoomerangs(array1)); // Output: 
